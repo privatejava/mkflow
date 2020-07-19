41122c24444b07e0ee4867b108cfa3717a66bf26
Narayan G <privatejava@yahoo.com>
2020-07-19 22:01:56 +0545

    alternative of objectmaper

--- src/main/java/com/mkflow/config/AutoJacksonModule.java
+++ src/main/java/com/mkflow/config/AutoJacksonModule.java
@@ -2,11 +2,24 @@ package com.mkflow.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.jackson.ObjectMapperCustomizer;
-import javax.inject.Singleton;
+import org.jboss.resteasy.plugins.providers.jackson.ResteasyJackson2Provider;
 
-@Singleton
-public class AutoJacksonModule implements ObjectMapperCustomizer {
-    public void customize(ObjectMapper mapper) {
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+@Consumes({"application/json", "application/*+json", "text/json"})
+@Produces({"application/json", "application/*+json", "text/json"})
+public class AutoJacksonModule extends ResteasyJackson2Provider {
+    private ObjectMapper mapper = new ObjectMapper(); // my own object mapper
+
+
+    @Override
+    public ObjectMapper locateMapper(Class<?> type, MediaType mediaType) {
         mapper.findAndRegisterModules();
+        return mapper;
     }
 }
