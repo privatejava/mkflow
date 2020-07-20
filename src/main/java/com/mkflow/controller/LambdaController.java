38510fd89bf816d891f612ec778a39c6a8ef8407
Narayan G <privatejava@yahoo.com>
2020-07-20 22:06:27 +0545

    using environment variable and system property

--- src/main/java/com/mkflow/controller/LambdaController.java
+++ src/main/java/com/mkflow/controller/LambdaController.java
@@ -1,18 +1,5 @@
 package com.mkflow.controller;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.mkflow.service.HookHandlerService;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.inject.Inject;
-import javax.inject.Named;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * @author Narayan <me@ngopal.com.np> - ngm
  * Created 20/07/2020 17:14
