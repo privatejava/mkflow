ba30bc8c74ce955c80d66b15780b49a689f85389
Narayan G <privatejava@yahoo.com>
2020-07-20 19:40:06 +0545

    no more lambda

--- src/main/java/com/mkflow/controller/LambdaController.java
+++ src/main/java/com/mkflow/controller/LambdaController.java
@@ -17,21 +17,21 @@ import java.util.Map;
  * @author Narayan <me@ngopal.com.np> - ngm
  * Created 20/07/2020 17:14
  **/
-@Named("jobHandler")
-public class LambdaController implements RequestHandler<Map, Map> {
+//@Named("jobHandler")
+public class LambdaController { /*implements RequestHandler<Map, Map> {*/
 
-	private static Logger log = LoggerFactory.getLogger(LambdaController.class);
-
-	@Inject
-	HookHandlerService service;
-
-	@Override
-	public Map handleRequest(Map json, Context context) {
-		try {
-			return service.process(json);
-		} catch (IOException  | GitAPIException e) {
-			e.printStackTrace();
-			return new HashMap();
-		}
-	}
+//	private static Logger log = LoggerFactory.getLogger(LambdaController.class);
+//
+//	@Inject
+//	HookHandlerService service;
+//
+//	@Override
+//	public Map handleRequest(Map json, Context context) {
+//		try {
+//			return service.process(json);
+//		} catch (IOException  | GitAPIException e) {
+//			e.printStackTrace();
+//			return new HashMap();
+//		}
+//	}
 }
